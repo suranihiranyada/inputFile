4e25edb85267fe0e44edc5eb74bc812386fe97fe
suranihiranyada <suranihiranyada@gmail.com>
2016-05-03 07:52:27 +0530

    changed

---
+++ src/inputfile/Inputfile.java
@@ -0,0 +1,50 @@
+
+package inputfile;
+
+import java.io.*;
+import java.util.*;
+public class Inputfile {
+    private Formatter x;
+    private Scanner y;
+    
+    public void create_writefile(){
+        try {
+            x = new Formatter("in.txt");
+            x.format("%s","\n");
+            x.format("%s %s %s",11,2,3);
+            x.close();
+        } 
+        catch (FileNotFoundException ex) {
+            System.out.println("oops!");
+        }
+    }
+    public String[] read(){
+        String arr[]=new String[3];
+        try {
+            y = new Scanner(new File("C:\\Users\\suranihiranyada\\My Documents\\input.txt"));
+            int i=0;
+            while(y.hasNext()){
+                String a = y.next();
+                System.out.println(a);
+                arr[i]=a;
+                i++;
+            }
+        } catch (FileNotFoundException ex) {
+           System.out.println("no");
+        }
+        
+    return arr;    
+    }
+    public static void main(String[] args) {
+        Inputfile is = new Inputfile();
+        File c=new File("C:\\Users\\suranihiranyada\\My Documents\\input.txt");
+        if(c.exists())
+            System.out.println(c.getName());
+        is.create_writefile();
+        String arr[]=is.read();
+        for (String x:arr){
+            System.out.println(x);
+        }
+    }
+    
+}
